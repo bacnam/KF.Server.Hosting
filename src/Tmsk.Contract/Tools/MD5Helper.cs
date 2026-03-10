b4ca22367924a747ea25f3c15426c77afc5aadf4
Bac Nam <bacnam7287@gmail.com>
2026-03-10 17:56:30 +0700

    add init

---
+++ src/Tmsk.Contract/Tools/MD5Helper.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace Server.Tools
+{
+    public class MD5Helper
+    {
+        /// <summary>
+        /// 获取字符串的MD5值(HEX编码)
+        /// </summary>
+        /// <param name="str">输入的字符串</param>
+        /// <returns></returns>
+        public static string get_md5_string(string str)
+        {
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(str);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                StringBuilder sb = new System.Text.StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
